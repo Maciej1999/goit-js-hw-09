0fb1470dfb3b6f4b69d988ec2ad77c4282f3f74d
Maciej1999 <maciej.sikora.1999@wp.pl>
2023-02-03 18:10:03 +0100

    End of promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,11 +5,35 @@ let step = document.querySelector('[name="step"]');
 let amount = document.querySelector('[name="amount"]');
 const submit = document.querySelector('.form');
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const createPromise = (position, delay) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+const submitHandler = event => {
+  event.preventDefault();
+  let targetDelay = Number(delay.value);
+  let targetStep = Number(step.value);
+  let targetAmount = Number(amount.value);
+
+  for (let i = 1; i <= targetAmount; i++) {
+    createPromise(i, targetDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    targetDelay += targetStep;
   }
-}
+};
+
+submit.addEventListener('submit', submitHandler);
